2e56df465fdf15e2448e92df9c91aa369c8dd2ca
nhgoodboy <806452359@qq.com>
2025-03-19 12:12:21 +0800

    修改

--- src/ft_chatglm_lora/train.sh
+++ src/ft_chatglm_lora/train.sh
@@ -3,13 +3,13 @@ lora_trainable="query_key_value,dense,dense_h_to_4h,dense_4h_to_h"
 modules_to_save="null"
 lora_dropout=0.1
 LR=2e-4
-model_name_or_path="./models--THUDM--chatglm-6b/snapshots/a8ede826cf1b62bd3c78bdfb3625c7c5d2048fbd"   # LLM底座模型路径，或者是huggingface hub上的模型名称
+model_name_or_path="./models--THUDM--chatglm-6b/snapshots/a05d5f967eb67d2503fb8ccab6b5e01713305184"   # LLM底座模型路径，或者是huggingface hub上的模型名称
 your_data_path="./datasets/PromptCBLUE/toy_examples"  # 填入数据集所在的文件夹路径
 your_checkpopint_path="./experiments/outputs/"  # 填入用来存储模型的路径
 
 peft_path=""  # 如果之前训练过，且存储了peft权重，则设置为peft权重的文件夹路径
 
-CUDA_VISIBLE_DEVICES=1 python src/ft_chatglm_lora/main.py \
+CUDA_VISIBLE_DEVICES=1 python main.py \
     --do_train \
     --train_file $your_data_path/train.json \
     --validation_file $your_data_path/dev.json \
