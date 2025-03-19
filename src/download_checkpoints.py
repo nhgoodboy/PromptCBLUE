c088fc3270cd2e7f5f5de23e81a23de97ce31ec8
nhgoodboy <806452359@qq.com>
2025-03-19 12:43:02 +0800

    下载修改

--- src/download_checkpoints.py
+++ src/download_checkpoints.py
@@ -1,6 +1,12 @@
 from huggingface_hub import snapshot_download
 
-repo_id = "THUDM/visualglm-6b"
+# repo_id = "THUDM/visualglm-6b"
+# downloaded = snapshot_download(
+#     repo_id,
+#     cache_dir="./",
+# )
+
+repo_id = "THUDM/chatglm-6b"
 downloaded = snapshot_download(
     repo_id,
     cache_dir="./",
